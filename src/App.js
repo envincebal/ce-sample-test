f9eb784f4087b9be63e384ba7b457a06cac045a1
envincebal <vincentyan1@gmail.com>
2018-09-23 23:36:36 -0700

    Updated title

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ class App extends Component {
 
   render() {
     return (<div className="App" >
-      <h1 className="title">HVAC Report </h1>
+      <h1 className="title">HVAC Report</h1>
       <h3 className="subtitle">(Month of April 2018)</h3>
       <div className="display-total-container">
         <DisplayCount
